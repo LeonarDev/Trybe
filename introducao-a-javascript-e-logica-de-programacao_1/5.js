90b6170e08567325c2275ed767fb149563050d27
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-25 19:20:16 -0300

    adicionando exercicios resolvidos

---
+++ introducao-a-javascript-e-logica-de-programacao_1/5.js
@@ -0,0 +1,10 @@
+// 5. Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. 
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.
+
+let triangulo = 180, a = 30, b = 40, c = 60;
+
+if (a + b + c == 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
