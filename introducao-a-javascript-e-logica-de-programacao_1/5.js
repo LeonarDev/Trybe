7f1047f31b11b0e79b16ee59069eddb018cab47e
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-26 14:58:50 -0300

    exercises 5 n 6

--- introducao-a-javascript-e-logica-de-programacao_1/5.js
+++ introducao-a-javascript-e-logica-de-programacao_1/5.js
@@ -1,9 +1,10 @@
 // 5. Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. 
 // Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.
+// Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus
 
-let triangulo = 180, a = 30, b = 40, c = 60;
+let angulo1 = 178, angulo2 = 1, angulo3 = 1;
 
-if (a + b + c == 180) {
+if (angulo1 + angulo2 + angulo3 == 180 && angulo1 > 0 && angulo2 > 0 && angulo3 > 0) {
   console.log(true);
 } else {
   console.log(false);
--- introducao-a-javascript-e-logica-de-programacao_1/6.js
+++ introducao-a-javascript-e-logica-de-programacao_1/6.js
@@ -4,5 +4,20 @@
   // Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
   // Exemplo: bishop -> diagonals
 
-  let chessPiece;
+let chessPiece = "quEEn";
 
+if (chessPiece.toLocaleLowerCase() == "bishop") {
+  console.log("bishop -> moves any number of vacant squares diagonally in a straight line");
+} else if (chessPiece.toLocaleLowerCase() == "pawn") {
+  console.log("pawn -> forward exactly one square, or optionally, two squares when on its starting square");
+} else if (chessPiece.toLocaleLowerCase() == "rook") {
+  console.log("rook -> forwards, backwards, left, or right in a straight line");
+} else if (chessPiece.toLocaleLowerCase() == "knight") {
+  console.log("knight -> diagonal from one corner of any 2×3 rectangle, making a 'L'");
+} else if (chessPiece.toLocaleLowerCase() == "queen") {
+  console.log("queen -> any number of vacant squares in any direction: forwards, backwards, left, right, or diagonally, in a straight line");
+} else if (chessPiece.toLocaleLowerCase() == "king") {
+  console.log("king -> moves exactly one vacant square in any direction: forwards, backwards, left, right, or diagonally; however, it cannot move to a square that is under attack by an opponent");
+} else {
+  console.log("Enter a valid chess piece");
+}
