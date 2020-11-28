24a679aada4874c8bce76a571062a4720b8f457b
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-28 13:45:08 -0300

    Exercise solved

--- Exercises/fundamentals/4.2/exercise-3.js
+++ Exercises/fundamentals/4.2/exercise-3.js
@@ -1,4 +1,4 @@
-// Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
@@ -7,4 +7,4 @@ for(let index = 0; index < numbers.length; index += 1) {
   sum += numbers[index];
 }
 
-console.log(sum);
+console.log(sum/numbers.length);
