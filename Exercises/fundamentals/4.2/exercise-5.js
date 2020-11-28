d724e0356ed3a7538ac6bc6d85f3b631ecf460b4
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-28 14:08:49 -0300

    Exercise solved

--- Exercises/fundamentals/4.2/exercise-5.js
+++ Exercises/fundamentals/4.2/exercise-5.js
@@ -1,10 +1,9 @@
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
 let highestValue = 0;
 
-for(let index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] > highestValue) {
     highestValue = numbers[index];
   }
