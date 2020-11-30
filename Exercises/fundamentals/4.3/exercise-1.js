10d28ebdc28089aa72ab2161569988734827c31d
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-30 12:22:10 -0300

    Exercise solved

--- Exercises/fundamentals/4.3/exercise-1.js
+++ Exercises/fundamentals/4.3/exercise-1.js
@@ -1,4 +1,4 @@
-// 1- Ffaça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+// 1- Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
 let n = 5;
 let star = '*'
--- Exercises/fundamentals/4.3/exercise-2.js
+++ Exercises/fundamentals/4.3/exercise-2.js
@@ -1,3 +1,8 @@
-// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+// 2- Imprima um triângulo retângulo com 5 asteriscos de base.
 
+let n = 5;
+let star = '*';
 
+for (let index = 1; index <= n; index += 1) {
+  console.log(star.repeat(index));
+}
