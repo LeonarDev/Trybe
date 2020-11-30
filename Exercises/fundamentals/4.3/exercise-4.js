ba8b54ff4eca81b2eff0bbccc59125cef363ab37
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-30 18:17:32 -0300

    Exercise unsolved

--- Exercises/fundamentals/4.3/exercise-4.js
+++ Exercises/fundamentals/4.3/exercise-4.js
@@ -1,2 +1,9 @@
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
 
+let n = 6
+let star = '*';
+let space = ' ';
+
+for (let index = 1; index <= n; index += 1) {
+    console.log(star.repeat(n-(n-index)));
+}
