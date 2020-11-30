69252e69feea21a02f4adfa10a66c740a0c81c5f
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-30 12:35:31 -0300

    Exercise solved

--- Exercises/fundamentals/4.3/exercise-3.js
+++ Exercises/fundamentals/4.3/exercise-3.js
@@ -1,2 +1,9 @@
 // 3- Agora inverta o lado do triângulo.
 
+let n = 5;
+let star = '*';
+let space = ' ';
+
+for (let index = 1; index <= n; index += 1) {
+  console.log(space.repeat(n-index)+(star.repeat(index)));
+}
