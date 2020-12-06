095bed16e2ceab38f5b61067f51d00e1bb2bdf7b
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-06 18:18:00 -0300

    Solved exercise 2

--- Exercises/fundamentals/5.3/script.js
+++ Exercises/fundamentals/5.3/script.js
@@ -41,3 +41,15 @@ function createDaysOfTheMonth() {
   };
 };
 createDaysOfTheMonth();
+
+function createButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  let newButtonID = 'btn-holiday';
+
+  newButton.innerHTML = buttonName;
+  newButton.id = newButtonID;
+  buttonContainer.appendChild(newButton);
+};
+
+createButton('Feriados');
