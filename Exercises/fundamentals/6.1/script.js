0597b17a83daa5e4a59ae75f6548c25cccb32e1a
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-04 18:31:00 -0300

    Exercise 1

---
+++ Exercises/fundamentals/6.1/script.js
@@ -0,0 +1,36 @@
+const states = [
+  { 'AC': 'Acre' },
+  { 'AL': 'Alagoas' },
+  { 'AP': 'Amapá' },
+  { 'AM': 'Amazonas' },
+  { 'BA': 'Bahia' },
+  { 'CE': 'Ceará' },
+  { 'DF': 'Distrito Federal' },
+  { 'ES': 'Espírito Santo' },
+  { 'GO': 'Goías' },
+  { 'MA': 'Maranhão' },
+  { 'MT': 'Mato Grosso' },
+  { 'MS': 'Mato Grosso do Sul' },
+  { 'MG': 'Minas Gerais' },
+  { 'PA': 'Pará' },
+  { 'PB': 'Paraíba' },
+  { 'PR': 'Paraná' },
+  { 'PE': 'Pernambuco' },
+  { 'PI': 'Piauí' },
+  { 'RJ': 'Rio de Janeiro' },
+  { 'RN': 'Rio Grande do Norte' },
+  { 'RS': 'Rio Grande do Sul' },
+  { 'RO': 'Rondônia' },
+  { 'RR': 'Roraíma' },
+  { 'SC': 'Santa Catarina' },
+  { 'SP': 'São Paulo' },
+  { 'SE': 'Sergipe' },
+  { 'TO': 'Tocantins' },
+],
+
+// let stateOptions = document.querySelector('#input-state');
+
+// for (let index = 0; index < states.length. index += 1) {
+//   document.createElement('option');
+//   document.createAttribute('value', state.index);
+// }
