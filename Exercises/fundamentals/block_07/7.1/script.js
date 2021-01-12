86f3b35296f7e68f420a8fbaaad490ceb61f7cc9
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-12 13:10:49 -0300

    Organizes submodules

---
+++ Exercises/fundamentals/block_07/7.1/script.js
@@ -0,0 +1,39 @@
+// PART I
+
+// Exercise 1
+function testingScope(scope) { 
+  if (escopo === true) { 
+    var ifScope = 'I must not be used outside my scope (if)';
+    ifScope = ifScope + ' great, I was used in the scope!';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'I must not be used outside my scope (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' What am I doing here ? :O');
+}
+
+testingScope(true);
+
+// Exercise II
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Your code here.
+
+console.log(oddsAndEvens);
+
+
+// PART II
+
+// Exercise I
+
+
+// Exercise II
+
+
+// Exercise III
+
+
+// Exercise IV
+
+
