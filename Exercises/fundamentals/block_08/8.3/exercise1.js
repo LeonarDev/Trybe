af5869afae9fc046f0e4bf204a71919615f0f8b4
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-20 19:23:12 -0300

    Solve exercise 1

--- Exercises/fundamentals/block_08/8.3/exercise1.js
+++ Exercises/fundamentals/block_08/8.3/exercise1.js
@@ -10,6 +10,9 @@ const arrays = [
 
 function flatten() {
   // write your code here
-}
+  return arrays.reduce((previousValue, currentValue) => previousValue.concat(currentValue), []);
+};
+
+// console.log(flatten());
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
