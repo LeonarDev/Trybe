6119614e9eaf39fb87f5a18297104ffd67ff2d87
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-21 12:57:42 -0300

    Solve exercise 2

--- Exercises/fundamentals/block_08/8.3/exercise2.js
+++ Exercises/fundamentals/block_08/8.3/exercise2.js
@@ -65,9 +65,12 @@ const books = [
   },
 ];
 
-
+// 2. Create a string with the names of all authors.
 function allNames() {
   // write your code here
+  return books
+  .map((book) => book.author.name)
+  .reduce((previousValue, currentValue) => `${previousValue} ${currentValue},`, 'Names:').replace(/,$/, '.');
 }
 
 assert.deepStrictEqual(allNames(), "Names: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.");
