c9959ae3e609c0dc40e19666cb5845de143d5a7a
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-25 11:42:10 -0300

    Solve exercise 11

--- Exercises/fundamentals/block_04/4.1/exercise11.js
+++ Exercises/fundamentals/block_04/4.1/exercise11.js
@@ -1,11 +1,47 @@
-// 5. Make a program that defines three variables with the values of the three internal angles of a triangle.
-// Return true if the angles represent the angles of a triangle and false otherwise.
-// For the angles to be a valid triangle, the sum of the three must be 180 degrees
+// 11. A person with a formal contract in Brazil has the INSS and the IR deducted from their gross salary. Make a program that, given a gross salary, calculates the net salary to be received by her.
 
-let angle1 = 178, angle2 = 1, angle3 = 1;
+// The notation for a salary of R$ 1500.10, for example, should be 1500.10. For tax ranges, use the following references:
 
-if (angle1 + angle2 + angle3 == 180 && angle1> 0 && angle2> 0 && angle3> 0) {
-   console.log (true);
+// INSS
+// - Gross salary up to R$ 1,556.94: 8% rate
+// - Gross salary from R$ 1,556.95 to R$ 2,594.92: 9% rate
+// - Gross salary from R$ 2,594.93 to R$ 5,189.82: 11% rate
+// - Gross salary above R$ 5,189.82: maximum rate of R$ 570.88
+
+// GO
+// - Up to R$ 1,903.98: exempt from income tax
+// - From R$ 1,903.99 to 2,826.65: rate of 7.5% and installment of R$ 142.80 to be deducted from tax
+// - From R$ 2,826.66 to R$ 3,751.05: rate of 15% and installment of R$ 354.80 to be deducted from tax
+// - From R$ 3,751.06 to R$ 4,664.68: rate of 22.5% and installment of R$ 636.13 to be deducted from tax
+// - Above R$ 4,664.68: rate of 27.5% and installment of R$ 869.36 to be deducted from tax.
+
+
+let aliquotINSS;
+let aliquotIR;
+let grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
 } else {
-   console.log (false);
+  aliquotINSS = 570.88;
 }
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = baseSalary * 0.075 - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = baseSalary * 0.15 - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = baseSalary * 0.225 - 636.13;
+} else {
+  aliquotIR = baseSalary * 0.275 - 869.36;
+};
+
+console.log("Salary: " + (baseSalary - aliquotIR));
