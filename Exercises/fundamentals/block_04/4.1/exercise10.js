a6ef00372c0e2bf963e3f6b93d420758f615b1ea
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-25 11:37:49 -0300

    Solve exercise 9 and 10

--- Exercises/fundamentals/block_04/4.1/exercise10.js
+++ Exercises/fundamentals/block_04/4.1/exercise10.js
@@ -1,11 +1,17 @@
-// 5. Make a program that defines three variables with the values of the three internal angles of a triangle.
-// Return true if the angles represent the angles of a triangle and false otherwise.
-// For the angles to be a valid triangle, the sum of the three must be 180 degrees
+// 10. Write a program that starts with two values in two different variables: the cost of a product and its sales value. From the values, calculate how much profit (sales value less the cost of the product) the company will have when selling 1,000 of these products.
+// Note that a 20% tax is levied on the cost of the product, which must be taken into account when calculating profit.
+// Your program should also issue an error message and shut down if any of its input values are less than zero.
+// The profit of a product is the result of subtracting the sale value from the cost of the product, and the 20% tax is also part of the cost value.
+// totalCostValue = costValue + taxAboutCost
+// profit = salesValue - TotalCost (profit of a product)
 
-let angle1 = 178, angle2 = 1, angle3 = 1;
+let costOfProduct = 1;
+let saleValue = 3;
 
-if (angle1 + angle2 + angle3 == 180 && angle1> 0 && angle2> 0 && angle3> 0) {
-   console.log (true);
+if (costOfProduct >= 0 && saleValue >= 0) {
+  let totalCostOfProduct = costOfProduct * 1.2;
+  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
 } else {
-   console.log (false);
-}
+  console.log("Error, values cannot be negative");
+};
