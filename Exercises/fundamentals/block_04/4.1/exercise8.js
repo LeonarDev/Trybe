8101be9a38486f4406b062acfd1aeef1e54958ac
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-25 11:34:35 -0300

    Solve exercise 8

--- Exercises/fundamentals/block_04/4.1/exercise8.js
+++ Exercises/fundamentals/block_04/4.1/exercise8.js
@@ -1,11 +1,12 @@
-// 5. Make a program that defines three variables with the values of the three internal angles of a triangle.
-// Return true if the angles represent the angles of a triangle and false otherwise.
-// For the angles to be a valid triangle, the sum of the three must be 180 degrees
+// 8. Write a program that defines three numbers in variables at the beginning and returns true if at least one of the three is even. Otherwise, it must return false.
 
-let angle1 = 178, angle2 = 1, angle3 = 1;
+let a = 1;
+let b = 3;
+let c = 5;
 
-if (angle1 + angle2 + angle3 == 180 && angle1> 0 && angle2> 0 && angle3> 0) {
-   console.log (true);
-} else {
-   console.log (false);
-}
+let isEven = false;
+
+if ((a % 2 == 0 || b % 2 == 0 || c % 2 == 0)) {
+  isEven = true;
+};
+console.log(isEven);
