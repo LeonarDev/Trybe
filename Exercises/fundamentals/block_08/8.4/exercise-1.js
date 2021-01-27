2eb034dd4ee2f76410b7ac53feedc2f89f1baf78
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-27 10:47:47 -0300

    Solve exercise 1

--- Exercises/fundamentals/block_08/8.4/exercise-1.js
+++ Exercises/fundamentals/block_08/8.4/exercise-1.js
@@ -10,5 +10,5 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 for(rectangle of rectangles) {
-  assert.strictEqual(rectangleArea(), rectangle[0]*rectangle[1]); // change the function call to rectangleArea
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0]*rectangle[1]); // change the function call to rectangleArea
 }
