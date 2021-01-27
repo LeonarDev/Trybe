edfab340316d9af330b5c59cc3502f12668927c9
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-27 00:22:06 -0300

    Start repository, update readme with summary of content and add exercise files

---
+++ Exercises/fundamentals/block_08/8.4/exercise-2.js
@@ -0,0 +1,11 @@
+// 2) Create a `sum` function that, given a variable number of elements, returns the sum of those elements. That is:
+
+const assert = require('assert');
+
+// type sum below
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
