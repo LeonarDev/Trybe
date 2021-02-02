410549b88f8e72034fe2517b1723d0d30d594b7d
LeoMajevski <leonardo.majevski@gmail.com>
2021-02-02 19:54:26 -0300

    Solve exercise 5

---
+++ Exercises/fundamentals/block_09/9.2/exercises/exercise-5.js
@@ -0,0 +1,24 @@
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+  const randomArray = Array.from(
+    { length: 10 },
+    () => Math.floor(Math.random() * 50) + 1
+  );
+  console.table(randomArray);
+
+  const sum = randomArray
+    .map(number => number * number)
+    .reduce((accumulator, number) => accumulator + number, 0);
+
+  (sum < 8000) ? resolve(): reject();
+  console.log(`Sum is: ${sum}`);
+});
+
+myPromise
+  .then(sum => [2, 3, 5, 10].map(number => sum / number))
+  .then(array => array.reduce((number, acc) => number + acc, 0))
+  .catch(() => console.log(`It's over 8000`));
+
+}
+
+fetchPromise();
