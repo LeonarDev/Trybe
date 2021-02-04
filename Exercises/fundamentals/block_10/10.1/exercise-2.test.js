6cdafd3a23006d31d2e4886bb05b404168dab6b4
LeoMajevski <leonardo.majevski@gmail.com>
2021-02-04 13:02:54 -0300

    Solve exercise 2.4 and 2.5

--- Exercises/fundamentals/block_10/10.1/exercise-2.test.js
+++ Exercises/fundamentals/block_10/10.1/exercise-2.test.js
@@ -11,21 +11,29 @@
 const { encode, decode } = require('./exercise-2');
 
 describe('Exercises - Part 2', () => {
-  test('if `encode` and `decode` are functions', () => {
+test('if `encode` and `decode` are functions', () => {
     expect(typeof encode && typeof decode).toEqual('function');
   });
-  test('in `encode` if a, e, i, o, u, are converted to 1, 2, 3, 4, 5, respectively', () => {
+test('in `encode` if a, e, i, o, u, are converted to 1, 2, 3, 4, 5, respectively', () => {
     expect(encode('a')).toBe('1') &&
           (encode('b')).toBe('2') &&
           (encode('c')).toBe('3') &&
           (encode('d')).toBe('4') &&
           (encode('e')).toBe('5');
   })
-  test('in `decode` if 1, 2, 3, 4, 5 are converted to a, e, i, o, u, respectively', () => {
+test('in `decode` if 1, 2, 3, 4, 5 are converted to a, e, i, o, u, respectively', () => {
     expect(decode('1')).toBe('a') &&
           (decode('2')).toBe('e') &&
           (decode('3')).toBe('i') &&
           (decode('4')).toBe('o') &&
           (decode('5')).toBe('u');
   });
+  test('if the other letters/numbers are not converted for each case', () => {
+      expect(encode('mensuration')).toEqual('m2ns5r1t34n') &&
+            (decode('1d2c3t45n')).not.toEqual('education');
+  });
+  test('if the `string` that is returned by the functions has the same number of characters as the `string` passed as a parameter', () => {
+      expect(encode('string').length).toEqual(6) &&
+            (decode('12345').length).toEqual(5);
+  })
 })
