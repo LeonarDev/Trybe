9708a123577368a01a435da559d2b907097416e0
LeonarDev <leonardo.majevski@gmail.com>
2021-02-23 16:30:25 -0300

    Solve exercise 6

---
+++ Exercises/fundamentals/block_10/10.2/exercise-6.test.js
@@ -0,0 +1,39 @@
+const getAnimalByName = require('./exercise-6');
+const getAnimalByAge = require('./exercise-6');
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimalByName('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimalByName('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+describe('Testando promise - findAnimalByAge', () => {
+  describe('Quando existe o animal com o a idade', () => {
+    test('Verifique o primeiro nome do animal no array retornado', () => {
+      expect.assertions(1);
+      const animals = [{ name: 'Preguiça', age: 5, type: 'Cat' }];
+      return expect(getAnimalByAge(5)).resolves.toEqual(animals);
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return expect(getAnimalByAge(3)).rejects.toBe('Nenhum animal encontrado!');
+    });
+  });
+});
