9932062a3e1a692df95308b4148405375a8b14e9
LeonarDev <leonardo.majevski@gmail.com>
2021-02-22 22:05:21 -0300

    Updates exercise description

--- Exercises/fundamentals/block_10/10.2/exercise-2.js
+++ Exercises/fundamentals/block_10/10.2/exercise-2.js
@@ -1,3 +1,6 @@
+// Using the Promise syntax, run a test that checks the result of the getUserName function for the case where the user is found, and also a test for the case where the user is not found.
+// Tip: See the false data used in the database, available in the users variable, to know which IDs exist.
+
 const users = {
   4: { name: 'Mark' },
   5: { name: 'Paul' }
