a0c5f7b4a69599d6c4d03af5679702de36ab5596
LeonarDev <leonardo.majevski@gmail.com>
2021-06-06 11:00:07 -0300

    Solve exercises

--- Exercises/back-end/block_22/22.3/exercises/exercise8.sql
+++ Exercises/back-end/block_22/22.3/exercises/exercise8.sql
@@ -1,1 +1,9 @@
-/* 8. Escreva uma query SQL para alterar o nome da coluna region_name para region , mantendo o mesmo tipo e tamanho de dados. */
+/* 8. Escreva uma query para alterar o nome da coluna region_name para region, 
+      mantendo o mesmo tipo e tamanho de dados. */
+USE hr;
+
+ALTER TABLE regions
+  CHANGE REGION_NAME
+    REGION VARCHAR(25);
+
+SELECT * FROM regions;
